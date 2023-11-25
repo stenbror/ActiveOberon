4b99c8a36edfbfca01de93a7f878a74779f10d6d
Richard Magnor Stenbro <stenbror@hotmail.com>
2023-11-25 18:29:02 +0100

    Unittests upto dot unequal passed.

--- tests/TestCompilerScanner_operators.cc
+++ tests/TestCompilerScanner_operators.cc
@@ -93,6 +93,152 @@ void operators_bar()
     ASSERT_EQUAL(symb.end_pos, 1);
 }
 
+void operators_comma()
+{
+  auto text = std::u32string(U",");
+    
+    auto lexer = std::make_shared<ActiveOberonScanner>(text);
+    auto symb = lexer->get_symbol();    
+
+    ASSERT_EQUAL(symb.symbol, Symbols::Comma);
+    ASSERT_EQUAL(symb.start_pos, 0);
+    ASSERT_EQUAL(symb.end_pos, 1);
+}
+
+void operators_transpose()
+{
+  auto text = std::u32string(U"`");
+    
+    auto lexer = std::make_shared<ActiveOberonScanner>(text);
+    auto symb = lexer->get_symbol();    
+
+    ASSERT_EQUAL(symb.symbol, Symbols::Transpose);
+    ASSERT_EQUAL(symb.start_pos, 0);
+    ASSERT_EQUAL(symb.end_pos, 1);
+}
+
+void operators_period()
+{
+  auto text = std::u32string(U".");
+    
+    auto lexer = std::make_shared<ActiveOberonScanner>(text);
+    auto symb = lexer->get_symbol();    
+
+    ASSERT_EQUAL(symb.symbol, Symbols::Period);
+    ASSERT_EQUAL(symb.start_pos, 0);
+    ASSERT_EQUAL(symb.end_pos, 1);
+}
+
+void operators_upto()
+{
+  auto text = std::u32string(U"..");
+    
+    auto lexer = std::make_shared<ActiveOberonScanner>(text);
+    auto symb = lexer->get_symbol();    
+
+    ASSERT_EQUAL(symb.symbol, Symbols::Upto);
+    ASSERT_EQUAL(symb.start_pos, 0);
+    ASSERT_EQUAL(symb.end_pos, 2);
+}
+
+void operators_colon()
+{
+  auto text = std::u32string(U":");
+    
+    auto lexer = std::make_shared<ActiveOberonScanner>(text);
+    auto symb = lexer->get_symbol();    
+
+    ASSERT_EQUAL(symb.symbol, Symbols::Colon);
+    ASSERT_EQUAL(symb.start_pos, 0);
+    ASSERT_EQUAL(symb.end_pos, 1);
+}
+
+void operators_semicolon()
+{
+  auto text = std::u32string(U";");
+    
+    auto lexer = std::make_shared<ActiveOberonScanner>(text);
+    auto symb = lexer->get_symbol();    
+
+    ASSERT_EQUAL(symb.symbol, Symbols::SemiColon);
+    ASSERT_EQUAL(symb.start_pos, 0);
+    ASSERT_EQUAL(symb.end_pos, 1);
+}
+
+void operators_and()
+{
+  auto text = std::u32string(U"&");
+    
+    auto lexer = std::make_shared<ActiveOberonScanner>(text);
+    auto symb = lexer->get_symbol();    
+
+    ASSERT_EQUAL(symb.symbol, Symbols::And);
+    ASSERT_EQUAL(symb.start_pos, 0);
+    ASSERT_EQUAL(symb.end_pos, 1);
+}
+
+void operators_not()
+{
+  auto text = std::u32string(U"~");
+    
+    auto lexer = std::make_shared<ActiveOberonScanner>(text);
+    auto symb = lexer->get_symbol();    
+
+    ASSERT_EQUAL(symb.symbol, Symbols::Not);
+    ASSERT_EQUAL(symb.start_pos, 0);
+    ASSERT_EQUAL(symb.end_pos, 1);
+}
+
+void operators_arrow()
+{
+  auto text = std::u32string(U"^");
+    
+    auto lexer = std::make_shared<ActiveOberonScanner>(text);
+    auto symb = lexer->get_symbol();    
+
+    ASSERT_EQUAL(symb.symbol, Symbols::Arrow);
+    ASSERT_EQUAL(symb.start_pos, 0);
+    ASSERT_EQUAL(symb.end_pos, 1);
+}
+
+void operators_questionmark()
+{
+  auto text = std::u32string(U"?");
+    
+    auto lexer = std::make_shared<ActiveOberonScanner>(text);
+    auto symb = lexer->get_symbol();    
+
+    ASSERT_EQUAL(symb.symbol, Symbols::QuestionMark);
+    ASSERT_EQUAL(symb.start_pos, 0);
+    ASSERT_EQUAL(symb.end_pos, 1);
+}
+
+void operators_not_equal()
+{
+  auto text = std::u32string(U"#");
+    
+    auto lexer = std::make_shared<ActiveOberonScanner>(text);
+    auto symb = lexer->get_symbol();    
+
+    ASSERT_EQUAL(symb.symbol, Symbols::NotEqual);
+    ASSERT_EQUAL(symb.start_pos, 0);
+    ASSERT_EQUAL(symb.end_pos, 1);
+}
+
+void operators_dot_un_equal()
+{
+  auto text = std::u32string(U".#");
+    
+    auto lexer = std::make_shared<ActiveOberonScanner>(text);
+    auto symb = lexer->get_symbol();    
+
+    ASSERT_EQUAL(symb.symbol, Symbols::DotUnEqual);
+    ASSERT_EQUAL(symb.start_pos, 0);
+    ASSERT_EQUAL(symb.end_pos, 2);
+}
+
+
+
 // Test harness for reserved keywords /////////////////////////////////////////
 
 int main() {
@@ -104,7 +250,20 @@ int main() {
   operators_left_brace();
   operators_right_brace();
   operators_bar();
-  
+  operators_comma();
+  operators_transpose();
+  operators_period();
+  operators_upto();
+  operators_colon();
+  operators_semicolon();
+  operators_and();
+  operators_not();
+  operators_arrow();
+  operators_questionmark();
+  operators_not_equal();
+  operators_dot_un_equal();
+
+
   return 0;
 }
 
