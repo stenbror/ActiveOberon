d548b87042d0df9e8514d3bb3fd09080c06bd069
Richard Magnor Stenbro <stenbror@hotmail.com>
2023-12-18 19:58:35 +0100

    Refresg of assembler coding

--- active_oberon_compiler/src/arm64_assembler.rs
+++
@@ -1,4 +0,0 @@
-
-// ActiveOberon Compiler, a native ARM v8 & X86-64 compiler / linker / builder utility.
-// Written by Richard Magnor Stenbro. Licensed under GPL v3
-// Arm64 Assembler module for compiling and linking of projects written in ActiveOberon language
