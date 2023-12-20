c005e7a9623fb5d991c07137ddc0898f677a1bc8
Richard Magnor Stenbro <stenbror@hotmail.com>
2023-12-20 16:28:19 +0100

    Unittests for ident and label

--- active_oberon_compiler/src/amd64_assembler.rs
+++ active_oberon_compiler/src/amd64_assembler.rs
@@ -8,7 +8,7 @@ enum AMD64Symbols {
     None,
     Ident(u32, u32, Box<String>),
     Label(u32, u32, Box<String>),
-    Number(u32, u32, Box<String>),
+    Number(u32, u32, i64),
     String_(u32, u32, Box<String>),
     Period(u32, u32),
     SemiColon(u32, u32),
@@ -31,41 +31,59 @@ enum AMD64Symbols {
     EndOfFile(u32)
 }
 
+enum AMD64Node {
+    None
+}
+
 
 
 pub trait AssemblerAMD64Methods {
-    fn new() -> Self;
+    fn new(text: Vec<char>, offset_position: u32) -> Self;
     fn get_position(&self) -> u32;
     fn get_char(&mut self) -> char;
     fn next_char(&mut self) -> ();
     fn skip_whitespace(&mut self) -> ();
     fn get_ident(&mut self) -> Box<String>;
-    fn get_number(&mut self) -> Box<String>;
+    fn get_number(&mut self) -> Result<i64, Box<String>>;
     fn get_string(&mut self) -> Box<String>;
     fn get_symbol(&mut self) -> Result<Box<AMD64Symbols>, Box<String>>;
+    fn factor(&mut self) -> Result<Box<AMD64Node>, Box<String>>;
+    fn term(&mut self) -> Result<Box<AMD64Node>, Box<String>>;
+    fn expression(&mut self) -> Result<Box<AMD64Node>, Box<String>>;
 }
 
 pub struct AssemblerAMD64 {
-
+    buffer: Vec<char>,	/* Sourcecode as a vector of chars */
+    start_pos: u32,		/* Start of current analyzed symbol */
+    index: u32			/* Position into vector */
 }
 
 impl AssemblerAMD64Methods for AssemblerAMD64 {
-    fn new() -> Self {
+    fn new(text: Vec<char>, offset_position: u32) -> Self {
         AssemblerAMD64 {
-
+            buffer: text,
+            start_pos: offset_position,
+            index: 0
         }
     }
 
     fn get_position(&self) -> u32 {
-        todo!()
+        self.index
     }
 
     fn get_char(&mut self) -> char {
-        todo!()
+        match self.buffer.get(self.index as usize) {
+            Some(x) => {
+                return x.clone()
+            },
+            _ => '\0'
+        }
     }
 
     fn next_char(&mut self) -> () {
-        todo!()
+        if self.index as usize <= self.buffer.len() - 1 {
+            self.index += 1;
+        }
     }
 
     fn skip_whitespace(&mut self) -> () {
@@ -93,15 +111,154 @@ impl AssemblerAMD64Methods for AssemblerAMD64 {
     }
 
     fn get_ident(&mut self) -> Box<String> {
-        todo!()
+        let mut text = String::new();
+        loop {
+            match self.get_char() {
+                '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | 'a' ..= 'z' | 'A' ..= 'Z' | '_' => {
+                    text.push(self.get_char())
+                },
+                _ => break
+            }
+            self.next_char();
+        }
+
+        Box::new(text)
     }
 
-    fn get_number(&mut self) -> Box<String> {
-        todo!()
+    fn get_number(&mut self) -> Result<i64, Box<String>> {
+        let mut text = String::new();
+        let mut value : i64 = 0;
+
+        loop {
+            match self.get_char() {
+                '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | 'a' ..= 'f' | 'A' ..= 'F' => {
+                    text.push(self.get_char());
+                    self.next_char();
+                },
+                _ => break
+            }
+        }
+
+        match self.get_char() {
+            'H' | 'X' => {
+                self.next_char();
+                for el in text.chars() {
+                    match el {
+                        '0' => {
+                            value = value * 0x10 + 0
+                        },
+                        '1' => {
+                            value = value * 0x10 + 1
+                        },
+                        '2' => {
+                            value = value * 0x10 + 2
+                        },
+                        '3' => {
+                            value = value * 0x10 + 3
+                        },
+                        '4' => {
+                            value = value * 0x10 + 4
+                        },
+                        '5' => {
+                            value = value * 0x10 + 5
+                        },
+                        '6' => {
+                            value = value * 0x10 + 6
+                        },
+                        '7' => {
+                            value = value * 0x10 + 7
+                        },
+                        '8' => {
+                            value = value * 0x10 + 8
+                        },
+                        '9' => {
+                            value = value * 0x10 + 9
+                        },
+                        'a' | 'A' => {
+                            value = value * 0x10 + 10
+                        },
+                        'b' | 'B' => {
+                            value = value * 0x10 + 11
+                        },
+                        'c' | 'C' => {
+                            value = value * 0x10 + 12
+                        },
+                        'd' | 'D' => {
+                            value = value * 0x10 + 13
+                        },
+                        'e' | 'E' => {
+                            value = value * 0x10 + 14
+                        },
+                        'f' | 'F' => {
+                            value = value * 0x10 + 15
+                        },
+                        _ => ()
+                    }
+                }
+            },
+            _ => {
+
+                for el in text.chars() {
+                    match el {
+                        '0' => {
+                            value = value * 0x10 + 0
+                        },
+                        '1' => {
+                            value = value * 0x10 + 1
+                        },
+                        '2' => {
+                            value = value * 0x10 + 2
+                        },
+                        '3' => {
+                            value = value * 0x10 + 3
+                        },
+                        '4' => {
+                            value = value * 0x10 + 4
+                        },
+                        '5' => {
+                            value = value * 0x10 + 5
+                        },
+                        '6' => {
+                            value = value * 0x10 + 6
+                        },
+                        '7' => {
+                            value = value * 0x10 + 7
+                        },
+                        '8' => {
+                            value = value * 0x10 + 8
+                        },
+                        '9' => {
+                            value = value * 0x10 + 9
+                        },
+                        _ => {
+                            return Err(Box::new(format!("Found hex digit in non hex number at position: '{}'", self.get_position())))
+                        }
+                    }
+                }
+            }
+        }
+
+        Ok(value)
     }
 
     fn get_string(&mut self) -> Box<String> {
-        todo!()
+        let mut text = String::new();
+        text.push(self.get_char());
+        self.next_char();
+
+        loop {
+            match self.get_char() {
+                '\'' => {
+                    text.push(self.get_char());
+                    break
+                },
+                '\0' => break,
+                _ => ()
+            }
+            self.next_char()
+        }
+
+        Box::new(text)
     }
 
     /// Assembler lexer for AMD64 Syntax
@@ -116,8 +273,7 @@ impl AssemblerAMD64Methods for AssemblerAMD64 {
                 match self.get_char() {
                     ':' => {
                         self.next_char();
-                        let label = *symbol + ":";
-                        Ok(Box::new(AMD64Symbols::Label(start_pos, self.get_position(), Box::new(label))))
+                        Ok(Box::new(AMD64Symbols::Label(start_pos, self.get_position(), symbol)))
                     },
                     _ => {
                         Ok(Box::new(AMD64Symbols::Ident(start_pos, self.get_position(), symbol)))
@@ -125,7 +281,7 @@ impl AssemblerAMD64Methods for AssemblerAMD64 {
                 }
             },
             '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' => {
-                let symbol = self.get_number();
+                let symbol = self.get_number()?;
                 Ok(Box::new(AMD64Symbols::Number(start_pos, self.get_position(), symbol)))
             },
             '\'' => {
@@ -206,9 +362,35 @@ impl AssemblerAMD64Methods for AssemblerAMD64 {
             },
             '\0' => {
                 Ok(Box::new(AMD64Symbols::EndOfFile(self.get_position())))
-            }
+            },
+            '\r' => {
+                self.next_char();
+                match self.get_char() {
+                    '\n' => {
+                        self.next_char()
+                    },
+                    _ => ()
+                }
+                self.get_symbol()
+            },
+            '\n' => {
+                self.next_char();
+                self.get_symbol()
+            },
             _ => Err(Box::new(format!("Invalid symbol in inline assembler at position: '{}'", self.get_position())))
         }
+    }
+
+    fn factor(&mut self) -> Result<Box<AMD64Node>, Box<String>> {
+        todo!()
+    }
+
+    fn term(&mut self) -> Result<Box<AMD64Node>, Box<String>> {
+        todo!()
+    }
+
+    fn expression(&mut self) -> Result<Box<AMD64Node>, Box<String>> {
+        todo!()
     }
 }
 
@@ -216,4 +398,43 @@ impl AssemblerAMD64Methods for AssemblerAMD64 {
 #[cfg(test)]
 mod tests {
 
+    use crate::amd64_assembler::{ AssemblerAMD64, AssemblerAMD64Methods, AMD64Symbols };
+
+    #[test]
+    fn test_assembler_amd64_lexer_ident() {
+        let source = "test".chars().collect();
+        let mut lexer = AssemblerAMD64::new(source, 0);
+        let res = lexer.get_symbol();
+
+        match res {
+            Ok( x ) => {
+                match *x {
+                    AMD64Symbols::Ident( 0, 4, t ) => {
+                        assert_eq!(*t, String::from("test"))
+                    },
+                    _ => { assert!(false) }
+                }
+            },
+            _ => assert!(false)
+        }
+    }
+
+    #[test]
+    fn test_assembler_amd64_lexer_label() {
+        let source = "_test1:".chars().collect();
+        let mut lexer = AssemblerAMD64::new(source, 0);
+        let res = lexer.get_symbol();
+
+        match res {
+            Ok( x ) => {
+                match *x {
+                    AMD64Symbols::Label( 0, 7, t ) => {
+                        assert_eq!(*t, String::from("_test1"))
+                    },
+                    _ => { assert!(false) }
+                }
+            },
+            _ => assert!(false)
+        }
+    }
 }
